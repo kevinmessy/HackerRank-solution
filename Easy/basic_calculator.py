a839b0ddc153debcd5fc5dfb315f30067f623a40
Kevin Messy <kevin.messy2012@gmail.com>
2014-04-27 10:16:36 +0800

    added basic calculator

---
+++ Easy/basic_calculator.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+
+a = float(input())
+b = float(input())
+print('%.2f' %(a + b))
+print('%.2f' %(a - b))
+print('%.2f' %(a * b))
+print('%.2f' %(a / b))
+print('%.2f' %(a // b))
